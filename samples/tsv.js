b8657156e0f572ff24b6f43efa1cac70193d9340
Max Janisse <max@dynamicrangelabs.com>
2021-04-02 16:13:41 -0700

    Removed test case Reverted to original formatting

--- samples/tsv.js
+++ samples/tsv.js
@@ -1,22 +1,7 @@
 const parse = require('..');
 const assert = require('assert')
 
-parse(
-`1	2	3
-a	b	c`, {
-  delimiter: '\t'
-}, function(err, data){
+parse( "1	2	3\ra	b	c", {delimiter: '\t'}, function(err, data){
   if(err) throw err;
   assert.deepStrictEqual(data, [ [ '1', '2', '3' ], [ 'a', 'b', 'c' ] ]);
-});
-
-parse(
-`"1"	"2"	"3"
-"a"	"b"	"c"`, {
-  delimiter: '\t',
-  quote: "\"",
-  columns: true
-}, function(err, data){
-  if(err) throw err;
-  assert.deepStrictEqual(data, [ { 1: "a", 2: "b", 3: "c" } ]);
 });
